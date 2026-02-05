19b7ad1c4cf3d511237e99669c71f2eb220c6c3c
rinshankaihou <madoka@umich.edu>
2026-02-04 22:28:56 -0600

    wip wp_load

--- src/implang_hoare_expr.v
+++ src/implang_hoare_expr.v
@@ -1,8 +1,9 @@
 From iris_simp_lang Require Import imp_notation stack_ra implang_expr lifting_expr.
+From iris Require Import gen_heap.
 
 Section wp_expr.
 
-Context `{!envGS val Σ} `{!gen_heap.gen_heapGS loc val Σ} `{!invGS_gen HasNoLc Σ}.
+Context `{!envGS val Σ} `{!gen_heapGS loc val Σ} `{!invGS_gen HasNoLc Σ}.
 
 Lemma wp_val E Q n :  Q (NumV n) ⊢ wp_expr E (Num n) Q.
 Proof.
@@ -30,12 +31,35 @@ Proof.
     - iFrame. by iApply "HQ".
 Qed.
 
-Lemma wp_binop E Q op e1 e2 v1 v2 v:
-    bin_op_eval op v1 v2 = Some v →
+Lemma wp_binop E Q op e1 e2:
     wp_expr E e1 (λ v1,
       wp_expr E e2 (λ v2,
-        Q v)) 
+        match bin_op_eval op v1 v2 with
+        | Some v => Q v
+        | None => False
+        end)) 
     ⊢ wp_expr E (BinOp op e1 e2) Q.
+Proof.
+    intros. rewrite /wp_expr.
+    apply bi.forall_mono => σ.
+    apply bi.forall_mono => ρ.
+    apply bi.wand_mono; try done.
+    iIntros ">(%v1 & He1 & interp & He2)".
+    iMod ("He2" $! _ _ with "[interp]") as "(%v2 & He2 & $ & HQ)"; first done.
+    iModIntro.
+    destruct (bin_op_eval op v1 v2) eqn:?; last done.
+    iFrame.
+    iIntros (r) "env".
+    iPoseProof ("He1" with "env") as "%He1".
+    iPoseProof ("He2" with "env") as "%He2".
+    iPureIntro; econstructor; eauto.
+Qed.
+
+Notation "l ↦ v" := (pointsto(Σ:=Σ) l (DfracOwn 1) v) (at level 20).
+
+Lemma wp_load E (Q: val -> assert) op e v:
+    wp_expr E e (λ l, ∃ v, ⎡l ↦ v⎤ ∗ (⎡l ↦ v⎤ -∗ Q v))
+    ⊢ wp_expr E (UnOp op e) Q.
 Proof.
 
 End wp_expr.
