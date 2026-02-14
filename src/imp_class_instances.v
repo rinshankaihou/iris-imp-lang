49cba54edd61a5e31b7ca44e7fc816329d4fa790
rinshankaihou <madoka@umich.edu>
2026-02-14 00:21:39 -0600

    a bit more fixes

--- src/imp_class_instances.v
+++ src/imp_class_instances.v
@@ -8,15 +8,20 @@ Section class_instances_assert_of.
   Context `{!envGS Ty Σ}.
 
   Lemma bi_intuitionistically_if_assert_of p (P : nat → (iPropI Σ)) :
-    □?p (assert_of (λ v, P v)) ⊢ assert_of (λ v, □?p (P v)).
+    □?p (assert_of (λ v, P v)) ⊣⊢ assert_of (λ v, □?p (P v)).
   Proof. split => ?. rewrite monPred_at_intuitionistically_if //. Qed.
   
   Lemma assert_of_sep (P Q : nat → (iPropI Σ)) :
     assert_of (λ v, P v) ∗ assert_of (λ v, Q v) ⊣⊢ assert_of (λ v, P v ∗ Q v).
   Proof. split => ?. rewrite monPred_at_sep //. Qed.
   
-  Lemma assert_of_mono P Q : (∀ v, P v ⊢ Q v) → assert_of (λ v, P v) ⊢ assert_of (λ v, Q v).
+  Lemma assert_of_mono P Q :
+    (∀ v, P v ⊢ Q v) → assert_of (λ v, P v) ⊢ assert_of (λ v, Q v).
   Proof. split => ?. simpl. rewrite H //. Qed.
+
+  Lemma assert_of_and (P Q : nat → (iPropI Σ)) :
+    assert_of (λ v, P v) ∧ assert_of (λ v, Q v) ⊣⊢ assert_of (λ v, P v ∧ Q v).
+  Proof. split => ?. rewrite monPred_at_and //. Qed.
 
   Global Instance maybe_combine_sep_as_assert_of : forall Q1 Q2 P progress,
     MaybeCombineSepAs Q1 Q2 P progress →
@@ -30,7 +35,8 @@ Section class_instances_assert_of.
     rewrite monPred_at_sep monPred_at_persistently //.
   Qed.
 
-  Lemma combine_sep_assert_of_test P Q : ⊢ (assert_of (λ n, P)) -∗ (assert_of (λ n, Q)) -∗ assert_of (λ n, P ∗ Q).
+  Lemma combine_sep_assert_of_test P Q :
+    ⊢ (assert_of (λ n, P)) -∗ (assert_of (λ n, Q)) -∗ assert_of (λ n, P ∗ Q).
   Proof. iIntros "x y". by iCombine "x y" as "x". Qed.
 
 End class_instances_assert_of.
@@ -39,17 +45,17 @@ End class_instances_assert_of.
 Section class_instances_up1_down1.
   Context `{!envGS Ty Σ}.
   
-  Instance from_sep_up1 : forall P Q, FromSep (⇑ (P ∗ Q))%I (⇑ P) (⇑ Q).
+  Global Instance from_sep_up1 : forall P Q, FromSep (⇑ (P ∗ Q))%I (⇑ P) (⇑ Q).
   Proof. intros; rewrite /FromSep -up1_sep //. Qed.
   
-  Instance from_and_up1 : forall P Q, FromAnd (⇑ (P ∧ Q))%I (⇑ P) (⇑ Q).
+  Global Instance from_and_up1 : forall P Q, FromAnd (⇑ (P ∧ Q))%I (⇑ P) (⇑ Q).
   Proof. intros; rewrite /FromAnd -up1_and //. Qed.
   
-  Instance from_exists_up1 : forall {T:Type} (P: T → assert),
+  Global Instance from_exists_up1 : forall {T:Type} (P: T → assert),
     FromExist (∃ x, ⇑ (P x))%I (λ x, ⇑ (P x))%I.
   Proof. intros; rewrite /FromExist -up1_exist //. Qed.
   
-  Instance into_wand_up1 p q (R P Q: assert) : IntoWand p q R P Q → IntoWand p q (⇑ R)%I (⇑ P)%I (⇑ Q)%I.
+  Global Instance into_wand_up1 p q (R P Q: assert) : IntoWand p q R P Q → IntoWand p q (⇑ R)%I (⇑ P)%I (⇑ Q)%I.
   Proof. rewrite /IntoWand /up1. intros ?.
       split => ?. f_equiv. rewrite !bi_intuitionistically_if_assert_of.
       apply wand_intro_r.
@@ -60,15 +66,23 @@ Section class_instances_up1_down1.
       iIntros "[x y]"; by iApply "x". 
   Qed.
   
+  Global Instance into_and_up1 p P Q1 Q2 :
+    IntoAnd p P Q1 Q2 → IntoAnd p (⇑ P)%I (⇑ Q1)%I (⇑ Q2)%I. 
+  Proof. rewrite /IntoAnd /up1 => HP.
+    rewrite assert_of_and !bi_intuitionistically_if_assert_of /=.
+    apply assert_of_mono. intros v.
+    rewrite -monPred_at_and -!monPred_at_intuitionistically_if HP //.
+  Qed.
+
   (* what does FromAssumption/KnownLFromAssumption etc. do? *)
-  
-  Instance from_sep_down1 : forall P Q, FromSep (down1 (P ∗ Q))%I (down1 P) (down1 Q).
+
+  Global Instance from_sep_down1 : forall P Q, FromSep (down1 (P ∗ Q))%I (down1 P) (down1 Q).
   Proof. intros; rewrite /FromSep -down1_sep //. Qed.
   
-  Instance from_and_down1 : forall P Q, FromAnd (down1 (P ∧ Q))%I (down1 P) (down1 Q).
+  Global Instance from_and_down1 : forall P Q, FromAnd (down1 (P ∧ Q))%I (down1 P) (down1 Q).
   Proof. intros; rewrite /FromAnd -down1_and //. Qed.
   
-  Instance from_exists_down1 : forall {T:Type} (P: T → assert),
+  Global Instance from_exists_down1 : forall {T:Type} (P: T → assert),
     FromExist (∃ x, down1 (P x))%I (λ x, down1 (P x)).
   Proof. intros; rewrite /FromExist -down1_exist //. Qed.
   
